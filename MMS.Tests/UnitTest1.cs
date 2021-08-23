a1ad07744465e06e24b0bd76532e88b06356bea7
DavidTzur1 <david.tzur1@gmail.com>
2021-08-23 12:52:05 +0300

    Change project name to MMSC

--- MMS.Tests/UnitTest1.cs
+++ MMS.Tests/UnitTest1.cs
@@ -11,9 +11,9 @@ namespace MMS.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            //byte[] pdu = TestHelper.FileToByteArray(@"C:\Users\dtzur\source\repos\MMS\MMS.Tests\Trace\T178cad0e416.txt");
+            byte[] pdu = TestHelper.FileToByteArray(@"C:\Users\dtzur\source\repos\MMS\MMS.Tests\Trace\T178cad0e416.txt");
 
-            /*------------------------------------*
+            /*------------X-Mms-Expiry----------------*
             880680 044019FE91 is decoded as: 
             0x88: X - Mms - Expiry
             0x06: # of data bytes (80044019FE91) 
@@ -21,7 +21,9 @@ namespace MMS.Tests
             0x04: # of data bytes (4019FE91) 
             0x4019FE91: Absolute date(in delta seconds)
             -----------------------------------------*/
-            byte[] pdu = new byte[] { 0x88, 0x06, 0x80, 0x04, 0x40, 0x19, 0xFE, 0x91 };
+            //byte[] pdu = new byte[] { 0x88, 0x06, 0x80, 0x04, 0x40, 0x19, 0xFE, 0x91 };
+
+
             MM1Decoder decoder = new MM1Decoder(pdu);
             MMSMessageModel message = decoder.Parse();
         }
